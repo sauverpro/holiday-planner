e4459785c34ccd226b0e5d2bbceef6281b026bc6
sauverpro <unsauvechriss@gmail.com>
2023-10-18 16:16:44 +0200

    adding cors

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const express = require("express");
 import mongoose from "mongoose";
 import MainRouter from "./src/routes";
-import multer from "multer";
-
+import cors from 'cors'
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(cors())
 app.use(bodyParser.json());
 app.use('/api/v1',MainRouter)
 mongoose.connect(process.env.DB_CONNECT).then((res) => {

d125692f2da8e8a72eb777eb8ec251d675ec8f4c
sauverpro <unsauvechriss@gmail.com>
2023-10-19 10:36:31 +0200

    controller to add tour

--- src/controllers/addTour.controller.js
+++ src/controllers/addTour.controller.js
@@ -1,8 +1,7 @@
-import { tourModel } from "../models/tours.models";
+import { tourModel } from "../models";
 import { v2 as cloudinary } from "cloudinary";
-
+require("dotenv").config();
 const { CLOUD_NAME, API_KEY, API_SECRETE } = process.env;
-
 cloudinary.config({
   cloud_name: CLOUD_NAME,
   api_key: API_KEY,
@@ -11,11 +10,24 @@ cloudinary.config({
 export const addTour = async (req, res) => {
   try {
     const image = await cloudinary.uploader.upload(req.file.path);
-    console.log("images", image);
-    const tour = req.body;
-    console.log(req.file);
-    console.log("this is tour", tour);
+    req.body.backdropImage = image.secure_url;
+    const tour = req.body
+    
+    // console.log("this is tour", tour);
+  let add = await tourModel.create(tour);
+  if (!add) {
+   return res.status(404).json({
+      message : "failed to add tour"
+    })
+  }
+  res.status(201).json({
+      message : "Tour saved successfully",
+      data: tour
+    })
   } catch (error) {
     console.log(error);
+    res.status(500).json({
+      message : "internal server error",
+    })
   }
 };

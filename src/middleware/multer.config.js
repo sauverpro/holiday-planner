ffb6b8b9675f844fcca6763a7686210e7cd97ea5
sauverpro <unsauvechriss@gmail.com>
2023-10-19 10:43:44 +0200

    middleware to configure multer

--- src/middleware/multer.config.js
+++ src/middleware/multer.config.js
@@ -1,11 +1,12 @@
 import multer from "multer";
 const storage = multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null,"tour_asset_images");
-    },
-    filename: (req,file,cb)=>{
-        cb(null,file.originalname)
-    },
-})
-const upload = multer({dest:'tour_asset_images',storage:storage})
+  destination: (req, file, cb) => {
+    cb(null, "tour_asset_images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ dest: "tour_asset_images", storage: storage });
 export const uploads = upload.single('backdropImage')
+// export const uploadMany = upload.fields([{}]);

126feaf9bd2371b91e7163c5c24fbcde4a4b3f69
sauverpro <unsauvechriss@gmail.com>
2023-10-19 10:46:43 +0200

    tours route

--- src/routes/tours.routes.js
+++ src/routes/tours.routes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { addTour } from "../controllers";
-import { uploads } from "../middleware";
+import { addTour, deleteTour, getTours, uploadController } from "../controllers";
+import { uploads,uploadMany } from "../middleware";
 const tourRoute = express.Router();
 
+tourRoute.delete('/delete-Tour/:id',deleteTour)
 tourRoute.post('/addTour',uploads,addTour)
+// tourRoute.post('/addTour',uploadController)
+tourRoute.get('/',getTours)
 
 export default tourRoute

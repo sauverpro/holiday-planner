78e07fae10a3092baf2d29d3b6ecebd74006eb93
sauverpro <unsauvechriss@gmail.com>
2023-10-20 09:32:24 +0200

    modifying tour route

--- src/routes/tours.routes.js
+++ src/routes/tours.routes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { addTour, deleteTour, getTours, updateTour, uploadController } from "../controllers";
+import { addTour, deleteTour, getTours, updateTour, updateTours, uploadController } from "../controllers";
 import { uploads,uploadMany, verifyToken, isAdmin, } from "../middleware";
 import upload from "../middleware/multer.js"
 const tourRoute = express.Router();
 // ................
 
 tourRoute.delete('/delete-Tour/:id',verifyToken,isAdmin,deleteTour)
-tourRoute.patch('/update-Tour/:paramid',verifyToken,isAdmin,uploads,updateTour)
+tourRoute.patch('/update-Tour/:paramid',verifyToken,isAdmin,upload,updateTours)
 tourRoute.post('/addTour',upload,addTour)
 // tourRoute.post('/addTour',upload,uploadController)
 tourRoute.get('/',getTours)

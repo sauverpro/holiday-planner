5f15c58d996b8049ad13144329e639155367efac
sauverpro <unsauvechriss@gmail.com>
2023-10-24 10:32:05 +0200

    adding swagger doc for tours

--- src/routes/tours.routes.js
+++ src/routes/tours.routes.js
@@ -61,14 +61,8 @@ const tourRoute = express.Router();
  *  name: Tours
  *  description: The Tours managing API's
  */
-tourRoute.delete("/delete-Tour/:id", verifyToken, isAdmin, deleteTour);
-tourRoute.patch(
-  "/update-Tour/:paramid",
-  verifyToken,
-  isAdmin,
-  upload,
-  updateTours
-);
+
+
 /**
  * @swagger
  * /api/v1/Tours/addTour:
@@ -123,8 +117,8 @@ tourRoute.patch(
  *    security:
  *       - bearerAuth: []
  *    responses:
- *      200:
- *        description: You have successfully logged in
+ *      201:
+ *        description: Tour added successfully
  *        content:
  *          application/json:
  *            schema:
@@ -133,7 +127,132 @@ tourRoute.patch(
  *        description: internal server error
  *
  */
+
 tourRoute.post("/addTour", verifyToken, isAdmin, upload, addTour);
-// tourRoute.post('/addTour',upload,uploadController)
+
+/**
+ * @swagger
+ * /api/v1/Tours:
+ *  get:
+ *    summary: returns all registered users
+ *    tags: [Tours]
+ *    responses:
+ *        200:
+ *          description: the list of all users
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                     schema:
+ *                         type: array
+ *                         items:
+ *                          $ref: '#/components/schemas/Tour'
+ */
 tourRoute.get("/", getTours);
+
+/**
+ * @swagger
+ * /api/v1/Tours/update-Tour/{tourID}:
+ *  patch:
+ *    summary: -Update Tour
+ *    tags: [Tours]
+ *    parameters:
+ *      - in: path
+ *        name: tourID
+ *        required: true
+ *        schema:
+ *          type: string
+ *          description: The tour id to be updated
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        multipart/form-data:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              destination:
+ *                type: string
+ *              backdropImage:
+ *                type: file
+ *                items:
+ *                    type: string
+ *                    format: binary
+ *              Title:
+ *                type: string
+ *              Description:
+ *                type: string
+ *              Duration:
+ *                type: string
+ *              GroupTize:
+ *                type: string
+ *              Price:
+ *                type: string
+ *              Discount:
+ *                type: string
+ *              TourType:
+ *                type: string
+ *              Departure:
+ *                type: string
+ *              Seats:
+ *                type: string
+ *              fromMonth:
+ *                type: string
+ *              toMonth:
+ *                type: string
+ *              departureTime:
+ *                type: string
+ *              ReturnTime:
+ *                type: string
+ *              gallery:
+ *                  type: array
+ *                  items:
+ *                      type: file
+ *                      format: binary
+ *    security:
+ *       - bearerAuth: []
+ *    responses:
+ *      201:
+ *        description: You have successfully updated tour
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Tour'
+ *      500:
+ *        description: internal server error
+ *
+ */
+
+tourRoute.patch(
+  "/update-Tour/:tourID",
+  verifyToken,
+  isAdmin,
+  upload,
+  updateTours
+);
+// tourRoute.post('/addTour',upload,uploadController)
+
+
+/**
+ * @swagger
+ * /api/v1/Tours/delete-Tour/{id}:
+ *  delete: 
+ *    tags: [Tours]
+ *    summary: Delete tour by ID
+ *    security:
+ *      - bearerAuth: []
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        required:  true
+ *        schema:
+ *         type: string
+ *         description: Id of  tour to be deleted
+ *    responses:
+ *      200:
+ *        description: tour deleted successfully
+ *      404:
+ *        description: failed to deleted tour
+ * 
+ */ 
+tourRoute.delete("/delete-Tour/:id", verifyToken, isAdmin, deleteTour);
+
 export default tourRoute;

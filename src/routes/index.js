9ed7305793b7ebfd59b4a5f4626cbe3a0f510fec
sauverpro <unsauvechriss@gmail.com>
2023-10-24 10:29:21 +0200

    modigying routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import AuthRouter from "./auth.routes";
 import tourRoute from "./tours.routes";
+import bookingRoute from "./booking.routes";
 const MainRouter  = express.Router()
 MainRouter.use('/auth',AuthRouter)
 MainRouter.use('/Tours',tourRoute)
+MainRouter.use('/booking',bookingRoute)
 export default MainRouter

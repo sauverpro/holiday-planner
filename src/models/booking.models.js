f9704d8360112895c157fe343f0618f79f5e6819
sauverpro <unsauvechriss@gmail.com>
2023-10-24 10:37:06 +0200

    adding booking model

--- src/models/booking.models.js
+++ src/models/booking.models.js
@@ -1,1 +1,9 @@
-import mongoose from "mongoose";
+const  mongoose = require("mongoose");
+
+ const bookingSchema = mongoose.Schema({
+    userId: {type: String, required :  true},
+    tourId: {type: String, required :  true},
+    isPayed: {type: Boolean, default: false},
+    paymentMethod: {type: String, required :  true}
+})
+export const bookingModel = mongoose.model("booking",bookingSchema)

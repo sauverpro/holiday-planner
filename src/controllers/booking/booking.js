04a90a77286b0111c52a9c8993f7710b53d34c38
sauverpro <unsauvechriss@gmail.com>
2023-10-25 15:55:12 +0200

    Adding functionality to send email using nodemailer

--- src/controllers/booking/booking.js
+++ src/controllers/booking/booking.js
@@ -1,4 +1,6 @@
 import { UserModel, bookingModel, tourModel } from "../../models";
+import { transporter } from "../../utils";
+
 export const Booking = async (req, res) => {
   try {
     const { userId } = req;
@@ -23,12 +25,28 @@ export const Booking = async (req, res) => {
     });
     if (!addToBooking) {
       return res.status(404).json({
-        message: "failed to book please try again later",
+        message: "Failed to book please try again later",
       });
     }
-    res.status(201).json({
-      message:
-        "You have booked successfully now you can pay with your desired method",
+    // Email configuration
+    const mailOptions = {
+      from: "sauveur.dev@gmail.com",
+      to: checkUser.email,
+      subject: `Holiday Planer Booking`,
+      text: `Dear ${checkUser.FullNames},\n You have successfully Booked an travel to ${checkTour.destination}. Now you can Pay using you payment method`
+    };
+    console.log(mailOptions);
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.json({ success: false });
+      }
+      console.log("Email sent: " + info.response);
+
+      res.status(201).json({
+        message: "You have booked successfully now you can pay with your desired method",
+        success: true
+      });
     });
   } catch (error) {
     console.log(error);

e0396f92f416f23ec9f778b453d52dca575a1d40
sauverpro <unsauvechriss@gmail.com>
2023-10-25 15:55:36 +0200

    Adding functionality to send email using nodemailer

--- src/controllers/authentication/signup.js
+++ src/controllers/authentication/signup.js
@@ -1,28 +1,49 @@
 import { UserModel } from "../../models";
-import { hashPassword } from "../../utils";
- export const RegisterUser = async(req,res)=>{
-    try {
-        let checkUser = await UserModel.findOne({ email: req.body.email });
-        if (checkUser) {
-        return res.status(409).json({ message: "email already exist" });
-        }
-        let hash = await hashPassword(req.body.Password);
-        req.body.Password = hash;
-        let newUser = await UserModel.create(req.body);
-       
-    
-        res.status(200).json({
-          message: "User registered successfully",
-          data: {
-            email: newUser.email,
-            fullName: newUser.FullNames,
-            location: newUser.Location,
-          },
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "internal server error"
-        })
+import { hashPassword, transporter } from "../../utils";
+export const RegisterUser = async (req, res) => {
+  try {
+    let checkUser = await UserModel.findOne({ email: req.body.email });
+    if (checkUser) {
+      return res.status(409).json({ message: "email already exist" });
     }
- }
+    let hash = await hashPassword(req.body.Password);
+
+    let newUser = await UserModel.create({ ...req.body, Password: hash });
+
+    if (!newUser) {
+      return res.status(403).json({
+        message: "Oooops signup failed!! try again later",
+      });
+    }
+    // Email configuration
+    const mailOptions = {
+      from: "sauveur.dev@gmail.com",
+      to: newUser.email,
+      subject: `Welcome to event Planer platform`,
+      text: `Dear ${newUser.FullNames},\n You are most welcome to our platform. Now you can login with this credentials : \n Username: ${newUser.email}\n Password: ${req.body.Password}`,
+    };
+    console.log(mailOptions);
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        res.json({ success: false });
+      }
+      console.log("Email sent: " + info.response);
+
+      res.status(201).json({
+        message: "User registered successfully",
+        success: true,
+        data: {
+          email: newUser.email,
+          fullName: newUser.FullNames,
+          location: newUser.Location,
+        },
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "internal server error",
+    });
+  }
+};

3c4dc22b9f8bef8d9e25b10b65d6d0fb364c2c97
sauverpro <unsauvechriss@gmail.com>
2023-10-27 11:38:03 +0200

    adding contact routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import AuthRouter from "./auth.routes";
 import tourRoute from "./tours.routes";
 import bookingRoute from "./booking.routes";
+import contactRouter from "./contact.routes";
 const MainRouter  = express.Router()
 MainRouter.use('/auth',AuthRouter)
 MainRouter.use('/Tours',tourRoute)
 MainRouter.use('/booking',bookingRoute)
+MainRouter.use('/contact',contactRouter)
 export default MainRouter

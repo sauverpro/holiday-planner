8cf19b56c7f4d2b719bb05215c54998630020642
sauverpro <unsauvechriss@gmail.com>
2023-11-02 12:07:23 +0200

    adding swagger

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -1,5 +1,6 @@
 import express from "express"
-import { sendContact } from "../controllers/contact"
+import { getContacts, sendContact } from "../controllers/contact"
+import { isAdmin, verifyToken } from "../middleware"
 const contactRouter = express.Router()
 /**
  * @swagger
@@ -52,5 +53,31 @@ const contactRouter = express.Router()
  */
 
 contactRouter.post('/',sendContact)
+
+/**
+ * @swagger
+ * /api/v1/contact/contacts:
+ *   get:
+ *      summary: All Contacts
+ *      tags: [Contact]
+ *      security:
+ *          - bearerAuth: []
+ *      responses:
+ *          200:
+ *             description: All received contacts
+ *             content:
+ *                  application/json:
+ *                      schema:
+ *                          schema:
+ *                              type: array
+ *                              items:
+ *                                  $ref: '#/components/schemas/Contact'
+ *          404:
+ *            description: No contact found
+ *          500:
+ *            description: Internal server error
+ * 
+ */ 
+contactRouter.get('/contacts',verifyToken,isAdmin,getContacts)
 
 export default contactRouter
